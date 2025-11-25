302be6e128950ed295771ab9d30df0b287bebdd5
wafiqaghn <wafiqaghn@gmail.com>
2025-11-25 23:26:38 +0700

    Update village and edge data for graph initialization

--- VillagePaving.java
+++ VillagePaving.java
@@ -104,27 +104,35 @@ public class VillagePaving {
   public static final int RP_PER_METER = 5000;
 
   public static void main(String[] args) {
-    int V = 9;
-    int E = 15;
+    int V = 18;
+    int E = 23;
 
     int budget = 40000;
     Graph area = new Graph(V, E);
 
-    area.villages[0] = new VillageEdge('H', 'I', 1, 0);
-    area.villages[1] = new VillageEdge('D', 'E', 20, 12);
-    area.villages[2] = new VillageEdge('A', 'B', 2, 23);
-    area.villages[3] = new VillageEdge('E', 'I', 3, 2);
-    area.villages[4] = new VillageEdge('E', 'F', 3, 5);
-    area.villages[5] = new VillageEdge('A', 'C', 4, 7);
-    area.villages[6] = new VillageEdge('B', 'C', 4, 3);
-    area.villages[7] = new VillageEdge('B', 'F', 4, 5);
-    area.villages[8] = new VillageEdge('C', 'D', 5, 6);
-    area.villages[9] = new VillageEdge('F', 'H', 5, 12);
-    area.villages[10] = new VillageEdge('B', 'E', 6, 23);
-    area.villages[11] = new VillageEdge('E', 'H', 6, 2);
-    area.villages[12] = new VillageEdge('A', 'G', 7, 1);
-    area.villages[13] = new VillageEdge('F', 'G', 8, 2);
-    area.villages[14] = new VillageEdge('G', 'H', 9, 5);
+        area.villages[0] = new VillageEdge('P', 'A', 5, 80); 
+        area.villages[1] = new VillageEdge('P', 'B', 22, 65);
+        area.villages[2] = new VillageEdge('P', 'C', 13, 60);
+        area.villages[3] = new VillageEdge('P', 'D', 4, 55); 
+        area.villages[4] = new VillageEdge('P', 'E', 15, 57); 
+        area.villages[5] = new VillageEdge('P', 'I', 16, 75); 
+        area.villages[6] = new VillageEdge('P', 'J', 1, 10);
+        area.villages[7] = new VillageEdge('P', 'H', 4, 100);
+        area.villages[8] = new VillageEdge('I', 'H', 20, 100); 
+        area.villages[9] = new VillageEdge('J', 'I', 30, 70); 
+        area.villages[10] = new VillageEdge('J', 'H', 19, 95); 
+        area.villages[11] = new VillageEdge('J', 'U', 5, 71); 
+        area.villages[12] = new VillageEdge('H', 'K', 3, 100); 
+        area.villages[13] = new VillageEdge('H', 'U', 20, 89); 
+        area.villages[14] = new VillageEdge('E', 'K', 16, 60);
+        area.villages[15] = new VillageEdge('E', 'F', 21, 60); 
+        area.villages[16] = new VillageEdge('D', 'F', 7, 65); 
+        area.villages[17] = new VillageEdge('F', 'G', 8, 42); 
+        area.villages[18] = new VillageEdge('U', 'L', 10, 56); 
+        area.villages[19] = new VillageEdge('U', 'M', 17, 90); 
+        area.villages[20] = new VillageEdge('M', 'N', 13, 37); 
+        area.villages[21] = new VillageEdge('M', 'O', 11, 24); 
+        area.villages[22] = new VillageEdge('M', 'Q', 10, 43);
 
     VillagePavingOptimizer optimizer = new VillagePavingOptimizer(area, budget);
 
